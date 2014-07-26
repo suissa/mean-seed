7ed1e75cf8eb0007bf330947d642db69bb6ef1b8
Suissa <jnascimento@gmail.com>
2014-07-26 11:12:08 -0300

    Put Beer in a js module

---
+++ public/js/modules/beers/controllers.js
@@ -0,0 +1,229 @@
+'use strict';
+
+// Helpers
+var _beer = {
+  removeItem: function (arr, item){
+    var index = arr.indexOf(item);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+  },
+  cbFindSuccess: function (data, $scope) {
+      $scope.beers = data.data;
+      $scope.msg = 'List complete';
+      console.log(data);
+  },
+  cbFindError: function (error, $scope) {
+      $scope.status = 'Unable to load beers: ' + error.message;
+  }
+};
+
+// Controllers 
+angular.module('myApp.modules.Beer.controllers', []).
+  controller('AppCtrl', function ($scope, $http) {
+
+    $http({
+      method: 'GET',
+      url: '/api/name'
+    }).
+    success(function (data, status, headers, config) {
+      $scope.name = data.name;
+    }).
+    error(function (data, status, headers, config) {
+      $scope.name = 'Error!';
+    });
+
+  }).
+  controller('RemoveBeer', 
+    ['$scope', '$http', 'BeerService', 
+    function ($scope, $http, BeerService) {
+      var Beer = BeerService;
+    
+      $scope.remove = function(beer){
+        if(confirm('Are you sure? Beer '+beer.name+' will be removed.')){
+          Beer.remove(beer).
+          success(function (data) {
+            _beer.removeItem($scope.beers, beer);
+          }).
+          error(function (error) {
+              $scope.status = 'Unable to load beers: ' + error.message;
+          });
+
+        }
+    }
+  }]).
+  controller('BeerListController', 
+    ['$scope', '$http', 'BeerService', 
+    function ($scope, $http, BeerService) {
+
+      var Beer = BeerService;
+      Beer.find().then(function(data){
+        _beer.cbFindSuccess(data, $scope);
+      }, function(err){
+        _beer.cbFindError(err, $scope);
+      });
+
+  }]).
+  controller('BeerCreateController', 'BeerService', 
+    ['$scope', '$http',
+    function ($scope, $http, BeerService) {
+
+      var url = 'api/beers/';
+      var method = 'POST';
+      
+      $scope.message = 'Fill teh form bellow';
+
+      var Beer = BeerService;
+
+      $scope.create = function (data){
+        Beer.add().then(
+          function (data){
+          _beer.cbFindSuccess(data, $scope);
+        }, function(err){
+          _beer.cbFindError(err, $scope); 
+        });
+      }
+
+      // $scope.create = function(beer){     
+      //   $http({
+      //     url: url,
+      //     method: method,
+      //     data: beer
+      //   }).
+      //   success(function(data){
+      //     console.log(data);
+      //     $scope.data = data;
+      //     $scope.msg = 'Cerveja ' +beer.name+ ' cadastrada com sucesso'; 
+      //   }).
+      //   error(function(err){
+      //     console.log(err);
+      //     $scope.msg = 'Cerveja não pode ser cadastrada'; 
+      //   });
+      // }    
+
+  }]).
+  controller('BeerShowController', 
+    ['$scope', '$http', '$routeParams', '$location',
+    function ($scope, $http, $routeParams, $location) {
+
+      var id = $routeParams.id;
+      var url = 'api/beers/_id/'+id;
+      var method = 'GET';
+
+      $scope.message = 'Show Beer';
+
+      $http({
+        url: url,
+        method: method
+      }).
+      success(function(data){
+        console.log('Beer: ', data);
+        $scope.beer = data;
+      }).
+      error(function(err){
+        console.log(err);
+      });
+
+      $scope.remove = function(beer){
+        var id = beer._id;
+        var url = 'api/beers/'+id;
+        var method = 'DELETE';
+
+        $http({
+          url: url,
+          method: method
+        }).
+        success(function(data){
+          console.log(data);
+          $scope.beer = data;
+          $scope.message = 'Beer ' +beer.name+ 'removed successfully!';
+          $location.path('/beers');
+        }).
+        error(function(err){
+          console.log(err);
+          $scope.message = 'Beer cant be removed!';
+        });
+      }
+
+  }]).
+  controller('BeerEditController', 
+    ['$scope', '$http', '$routeParams',
+    function ($scope, $http, $routeParams) {
+
+      var id = $routeParams.id;
+      var url = 'api/beers/_id/'+id;
+      var method = 'GET';
+
+      $scope.message = 'Update Beer';
+      // Pega os valores da cerveja a ser alterada
+      $http({
+        url: url,
+        method: method
+      }).
+      success(function(data){
+        console.log(data);
+        $scope.beer = data;
+      }).
+      error(function(err){
+        console.log(err);
+      });
+
+      $scope.update = function(beer){
+
+        var method = 'PUT';
+
+        $http({
+          url: url,
+          method: method,
+          data: beer
+        }).
+        success(function(data){
+          console.log(data);
+          $scope.data = data;
+          $scope.msg = 'Beer ' +beer.name+ ' update successfully'; 
+        }).
+        error(function(err){
+          console.log(err);
+          $scope.msg = 'Beer cant be updated'; 
+        });
+      }
+  }]).
+  controller('BeerRemoveController', ['$scope', '$http', '$routeParams',
+    function ($scope, $http, $routeParams) {
+    $scope.title = 'Workshop Be MEAN';
+
+    var method = 'GET';
+    var id = $routeParams.id;
+    var url = '/api/beers/_id/'+id;
+    $http({
+      method: method,
+      url: url
+    })
+    .success(function(data){
+      $scope.beer = data;
+      $scope.msg = 'About ' + $scope.beer.name;
+    })
+    .error(function(data){
+      $scope.msg = 'Error in get beer';
+    });
+
+    $scope.remove = function(cerveja){
+      var method = 'DELETE';
+      var url = '/api/beers/_id/'+cerveja._id;
+      console.log(url);
+      if(confirm('Are you sure?')){
+        $http({
+          method: method,
+          url: url
+        })
+        .success(function(data){
+          $scope.msg = 'Beer ' +cerveja.name+ ' deleted successfully';
+        })
+        .error(function(data){
+          $scope.msg = 'ERROR on DELETE';
+        });
+
+      }
+    }
+
+  }]);
--- public/js/services.js
+++ public/js/services.js
@@ -4,55 +4,7 @@
 
 
 // Demonstrate how to register services
-angular.module('myApp.services', []).
-  value('version', '0.1').
-  factory('BeerFactory', ['$http',
-    function($http) {
-      var dataFactory = {};
-      var urlBase = 'api/beers'
-      dataFactory.find = function() {
-          return $http.get(urlBase);
-      };
-
-      dataFactory.findOne = function(id) {
-          return $http.get(urlBase + '/_id/' + id);
-      };
-
-      dataFactory.add = function(data) {
-          return $http.post(urlBase, data);
-      };
-
-      dataFactory.update = function(data) {
-          return $http.put(urlBase + '/_id/' + data.ID, data)
-      };
-
-      dataFactory.remove = function(data) {
-          return $http.delete(urlBase + '/_id/' + data.ID, data)
-      };
-      return dataFactory;
-    }
-  ]).
-  service('BeerService', ['$http',
-    function($http) {
-      var urlBase = 'api/beers';
-      this.find = function() {
-        return $http.get(urlBase);
-      };
-
-      this.findOne = function(id) {
-          return $http.get(urlBase + '/_id/' + id);
-      };
-
-      this.add = function(data) {
-          return $http.post(urlBase, data);
-      };
-
-      this.update = function(data) {
-          return $http.put(urlBase + '/_id/' + data._id, data)
-      };
-
-      this.remove = function(data) {
-          return $http.delete(urlBase + '/_id/' + data._id, data)
-      };
-    }
+angular.module('myApp.services', 
+  [
+    'myApp.modules.Beer.services'
   ]);
