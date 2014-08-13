633dbedd25767d93670b81308a60db17849d1929
Suissa <jnascimento@gmail.com>
2014-08-13 15:34:47 -0400

    redirecting to beers

--- back/modules/main/routes.js
+++ back/modules/main/routes.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('/main/views/index');
+  // res.render('/main/views/index');
+  res.redirect('/beers');
 });
 
 module.exports = router;
