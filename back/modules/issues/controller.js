aeff3d5f124d8c4bf7b2811c8e4213f5de268954
Rodrigo Gomes da Silva <rodrigo.smscom@gmail.com>
2015-02-13 15:26:33 -0200

    TeTest

--- back/modules/issues/controller.js
+++ back/modules/issues/controller.js
@@ -2,46 +2,46 @@ var Issue = require('./model');
 var msg = '';
 
 module.exports = {
-  renderList: function (req, res, cb) {
-    var msg = 'Listagem completa';
-    var view = 'Issues/list';
-    var query = {};
-    Issue.find(query, function (err, data) {
-      cb(err, data, res, view, msg);
-    });
-  },
-  renderCreate: function (req, res, cb) {
-    var msg = 'Cadastro de cerveja';
-    var view = 'Issues/create';
-    cb(null, null, res, view, msg);
-  },
-  renderShow: function (req, res, cb) {
-    var msg = 'Consulta completa';
-    var view = 'Issues/show';
-    var id = req.params.id;
-    var query = {_id: id};
-    Issue.findOne(query, function (err, data) {
-      cb(err, data, res, view, msg);
-    });
-  },
-  renderEdit: function (req, res, cb) {
-    var msg = 'Alteração de aplicação';
-    var view = 'Issues/edit';
-    var id = req.params.id;
-    var query = {_id: id};
-    Issue.findOne(query, function (err, data) {
-      cb(err, data, res, view, msg);
-    });
-  },
-  renderRemove: function (req, res, cb) {
-    var msg = 'Remoção de aplicação';
-    var view = 'Issues/remove';
-    var id = req.params.id;
-    var query = {_id: id};
-    Issue.findOne(query, function (err, data) {
-      cb(err, data, res, view, msg);
-    });
-  }
+    renderList: function (req, res, cb) {
+        var msg = 'Listagem completa';
+        var view = 'Issues/list';
+        var query = {};
+        Issue.find(query, function (err, data) {
+            cb(err, data, res, view, msg);
+        });
+    },
+    renderCreate: function (req, res, cb) {
+        var msg = 'Cadastro de problema';
+        var view = 'Issues/create';
+        cb(null, null, res, view, msg);
+    },
+    renderShow: function (req, res, cb) {
+        var msg = 'Consulta completa';
+        var view = 'Issues/show';
+        var id = req.params.id;
+        var query = {_id: id};
+        Issue.findOne(query, function (err, data) {
+            cb(err, data, res, view, msg);
+        });
+    },
+    renderEdit: function (req, res, cb) {
+        var msg = 'Alteração de problema';
+        var view = 'Issues/edit';
+        var id = req.params.id;
+        var query = {_id: id};
+        Issue.findOne(query, function (err, data) {
+            cb(err, data, res, view, msg);
+        });
+    },
+    renderRemove: function (req, res, cb) {
+        var msg = 'Remoção de problema';
+        var view = 'Issues/remove';
+        var id = req.params.id;
+        var query = {_id: id};
+        Issue.findOne(query, function (err, data) {
+            cb(err, data, res, view, msg);
+        });
+    }
 };
 
 
