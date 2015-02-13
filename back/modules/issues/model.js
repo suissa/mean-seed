3f671a9f959408eb2ffd1b68bf00ab7355cd2d82
Rodrigo Gomes da Silva <rodrigo.smscom@gmail.com>
2015-02-13 01:52:18 -0200

    Fixes

--- back/modules/issues/model.js
+++ back/modules/issues/model.js
@@ -2,14 +2,14 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var IssueSchema = new Schema({
-  hash: { type: String },
-  version: { type: String },
-  file: { type: String },
-  line: { type: Number },
-  address: { type: String },
-  message: { type: String },
-  additional: { type: String },
-  application: { type: Schema.Types.ObjectId, ref: 'Application' }
+    hash: { type: String },
+    version: { type: String },
+    file: { type: String },
+    line: { type: Number },
+    address: { type: String },
+    message: { type: String },
+    additional: { type: String },
+    application: { type: Schema.Types.ObjectId, ref: 'Application' }
 });
 
 module.exports = mongoose.model('Issue', IssueSchema);
